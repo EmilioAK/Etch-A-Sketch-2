27e1cd6034b7cc54d70eb101343ae326329fd283
Igor null <m1el.2027@gmail.com>
2021-11-25 20:10:52 +0200

    use a different algorithm to generate random color

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const gridSizeInput = document.getElementById('gridSize');
 
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  // Math.random() produces a fraction in range `[0, 1)`, then we multiply
+  // it by the range of colors (3 bytes), thus getting a uniform random value.
+  const randomColor = Math.floor(Math.random() * 0x1000000);
+  // We perform zero-padding by prepending a string of sufficient length,
+  // and then taking a substring from the end.
+  const padded = ('000000' + randomColor.toString(16)).substr(-6);
+  return '#' + padded;
 }
 
 function makeSquare() {
